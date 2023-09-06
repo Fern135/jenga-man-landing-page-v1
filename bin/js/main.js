4922d9dc7c9c10c99afe5b0557a73aa735711f2c
Fernando Camblor <35276153+Fern135@users.noreply.github.com>
2023-09-05 22:36:47 -0400

    the method i wrote to properly sleep the amount of time it takes for the gif to finish. isn't working so i got to do something else. maybe combine them into 1?

--- bin/js/main.js
+++ bin/js/main.js
@@ -1,94 +1,56 @@
+import 'gif.js';
+
+
 window.onload = LoadAll();
 
+const width        = window.innerWidth;
+const height       = window.innerHeight;
+
 async function LoadAll(){
-    try{
-        Promise.all(
-            [
-                
-            ]
-        )
-    }catch(error){
-        console.error(error.toString());
-    }
+  try{
+    Promise.all(
+      [
+        changeGifs()
+      ]
+    );
+  }catch(error){
+    console.error(error.toString());
+  }
+}
+
+function gifSpeed(element_id){
+  // const htmlImage = document.getElementById("jenga-man-loading");
+  
+  const gif = new GIF.parse(element_id); //TODO: does not work, find a diferent method that actually works.
+
+  const frameCount = gif.getFrameCount();
+  const delay      = gif.getDelay();
+
+  const duration = frameCount * delay;
+
+  // console.log(`The GIF takes ${duration} seconds to play.`);
+  return duration;
 }
 
 async function sleep(ms) {
-    await new Promise((resolve) => setTimeout(resolve, ms));
-  }
+  await new Promise((resolve) => setTimeout(resolve, ms));
+}
   
 
 function changeGifs(){
-    try{
-        let htmlImage = $("#jenga-man-loading");
-        const gifs = ['./bin/assets/img/jengman-adventure.gif' ]
-        const firstGifTime = 10000;
-        
-        htmlImage.attr('src', gifs[0]); //<=== going through space
-        sleep(firstGifTime); //<============== how long to sleep
-        htmlImage.attr('src', gifs[1]); //<=== landing and turning tree into bookshelf
-    }catch(error){
-        console.error(`error at changing gifs\nerror code: ${error}`);
-    }
+  try{
+    let htmlImage      = document.getElementById("jenga-man-loading");
+    const gifs         = ['../assets/img/jengman-adventure.gif', '../assets/img/Landing on Aura Planet v1.gif']
+    const firstGifTime = gifSpeed(htmlImage);
+
+    htmlImage.setAttribute('src', gifs[0]); //<================ going through space
+    htmlImage.setAttribute('alt', "Jengaman traveling"); //<=== going through space
+
+    sleep(firstGifTime); //<=================================== how long to sleep
+
+    htmlImage.setAttribute('src', gifs[1]); //<================ landing and turning tree into bookshelf
+    htmlImage.setAttribute('alt', "Jengaman landing"); //<===== landing and turning tree into bookshelf
+  }catch(error){
+      console.error(`error at changing gifs\nerror code: ${error}`);
+  }
 }
-
-function animateSpace(){
-    var canvas = document.getElementById('canvas');
-
-    // getting the size of the window screen 
-    var width       = window.innerWidth;
-    var height      = window.innerHeight;
-    canvas.width    = width;
-    canvas.height   = height;
-
-    animate(canvas);
-}
-
-// may not need
-function animate(canvas) {
-    var image = document.createElement('img');
-    image.src = 'spaceship.png'; // the ship flying 
-  
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  
-    var ctx = canvas.getContext('2d');
-  
-    var x = 0;
-    var y = 0;
-  
-    var speed = 5;
-  
-    var end_time = new Date().getTime() + 5000; //5 seconds in milliseconds
-  
-    function loop() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
-      ctx.drawImage(image, x, y);
-  
-      x += speed;
-      y += speed;
-  
-      if (x > canvas.width) {
-        x = 0;
-      } else if (x < 0) {
-        x = canvas.width;
-      }
-  
-      if (y > canvas.height) {
-        y = 0;
-      } else if (y < 0) {
-        y = canvas.height;
-      }
-  
-      if (new Date().getTime() > end_time) {
-        clearInterval(interval);
-      }
-  
-      requestAnimationFrame(loop);
-    }
-  
-    var interval = setInterval(loop, 1000/60); // 60 frames per second
-  
-    loop();
-}
-  
